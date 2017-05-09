d36317cb0881ca603b56cf1f14db57d637fa76be
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-05-09 17:05:16 +0200

    working on CRUD

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all  
+  end
+
+  def create 
+    Book.create title: params[:title]
+    redirect_to "/books"
+  end
+
+  def show 
+    @book = Book.find(params[:id])
+  end 
+
+  def update 
+    Book.find(params[:id].update title: params[:title])
+    redirect_to "/books/#{params[:id]}"
+  end
+  
+end
