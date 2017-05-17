f1a68f60b6b77c0728758ba8b58a7878d6a5b80f
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-05-17 18:40:28 +0200

    schema is being changed by new  category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504142709) do
+ActiveRecord::Schema.define(version: 20170517163220) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_books_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
 end
