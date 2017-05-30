c8abfbb1818cd25ba4594787d95a5f79ffb93d1a
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-05-30 16:31:42 +0200

    swaping to more relevant syntax

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,8 +1,5 @@
 module BooksHelper
-    def book_link the_book
-        html = "<a href='/books/#{the_book.id}'>".html_safe
-        html += the_book.title
-        html += "</a>".html_safe
-        hmtl
+    def book_link book
+        "<a href='/books/#{book.id}'>".html_safe + book.title + "</a>".html_safe
     end
 end
