e343c38bda5132238de5eec9da6aa3d143bfe18b
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-05-30 16:04:02 +0200

    creating a :french scope to sort category_id: 1

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,12 @@ class Book < ActiveRecord::Base
 
     validates :title, presence: {
         message: "Le titre doit être renseigné."
-    } 
+    }
+    
 
     validates :title, uniqueness: {
         message: "Ce titre existe déjà."
     }
+
+    scope :french, -> { where(category_id: 1) }
 end
