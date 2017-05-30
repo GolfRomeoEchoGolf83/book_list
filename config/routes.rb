d78121c13fb2701c19cf6691fefd1acf48209fab
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-05-30 17:44:36 +0200

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'books#index'
   get 'books' => 'books#index'
-  post 'books' => 'books#create'
   get 'books/:id' => 'books#show'
   patch 'books/:id' => 'books#update'
   delete 'books/:id' => 'books#destroy'
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'books' => 'books#create'
 end
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
