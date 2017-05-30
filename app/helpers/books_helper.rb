b535c78b8009a0adcaf8617964f782739ce7d9d2
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-05-30 16:26:50 +0200

    creation of book helper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,8 @@
 module BooksHelper
+    def book_link the_book
+        html = "<a href='/books/#{the_book.id}'>".html_safe
+        html += the_book.title
+        html += "</a>".html_safe
+        hmtl
+    end
 end
