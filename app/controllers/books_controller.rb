3fd43a9d318c31cce92c0437f0e4bab1e3dc3fb7
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-06-02 17:08:48 +0200

    adding a flash message in order to deal with successful update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,6 +16,7 @@ class BooksController < ApplicationController
   def update 
     @book = Book.find(params[:id])
     if @book.update title: params[:title]
+      flash[:success] = "Le livre a été mis à jour."
       redirect_to "/books/#{params[:id]}"
     else 
       render 'show'
