359d97c180c6436fadfd94af390f6a17114e872d
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-06-07 17:56:30 +0200

    oups routes deletion corrected

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   resources :books, only: [:index, :create, :show, :update, :destroy]
-end
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
